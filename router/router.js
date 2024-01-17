1f4cc2ec350ff481de681a689e89d2497bda5adf
asierpl <150433843+asierpl@users.noreply.github.com>
2024-01-17 21:04:19 +0100

    Update router.js

--- router/router.js
+++ router/router.js
@@ -11,4 +11,19 @@ router.route('/')
 router.route('/gestor')
     .get(getGestor)
 
+// router.route('/productos')
+//     .get(getProductos)
+
+// router.route('/alquiler')
+//     .get(getAlquiler)
+
+// router.route('/quienes-somos')
+//     .get(getQuienes)
+
+// router.route('/reparacion')
+//     .get(getReparacion)
+
+// router.route('/contacto')
+//     .get(getContacto)
+
 module.exports = { router }
